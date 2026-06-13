7b7717a9b0e3701dc4c73c9a020eb1222a18dc81
AlexandrAratsky <rus45671@yandex.ru>
2026-06-13 16:39:11 +0300

    chore: clean up next.config.ts by removing commented output settings

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-    // output: 'export',
-    // distDir: 'dist',
+const nextConfig: NextConfig = { 
   };
 
 export default nextConfig
